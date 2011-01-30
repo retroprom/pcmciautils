retroprom/pcmciautils

commit c96724af1baaa926cbcc8d8e5b19fd5ba339d962

Makefile: add short help target
To help people finding out about the debugtools by either running 'make
help' or having a look at the top of the Makefile itself.

Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

Makefile
@@ -20,6 +20,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 
+help:
+	@echo "Common targets:"
+	@echo "  all		- build the standard set of utilities"
+	@echo "  debugtools	- build the debugging utilities"
+
 # Set this to 'false' if you do not need the socket-startup script
 # 
 # You don't need it if the socket driver does not select

commit a728e69725d0efe865d1787f03795f283f3b00e9

Makefile: let plain 'make' be like 'make all' again
That behaviour was broken by c96724af1baaa926cbcc8d8e5b19fd5ba339d962
(Makefile: add short help target). Also, update the helptext accordingly.

Signed-off-by: Wolfram Sang <w.sang@pengutronix.de>
Signed-off-by: Dominik Brodowski <linux@dominikbrodowski.net>

Makefile
@@ -20,9 +20,11 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #
 
+default: all
+
 help:
-	@echo "Common targets:"
-	@echo "  all		- build the standard set of utilities"
+	@echo "Common targets ('*' is default):"
+	@echo "* all		- build the standard set of utilities"
 	@echo "  debugtools	- build the debugging utilities"
 
 # Set this to 'false' if you do not need the socket-startup script